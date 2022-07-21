bde5158014d6f3bcfc13f527d60d2557d9a33b6d
ruchira <htruchira@gmail.com>
2022-07-21 22:02:33 +0530

    user controller changes

--- src/main/java/com/company/ResidentRoomManagement/controller/UserController.java
+++ src/main/java/com/company/ResidentRoomManagement/controller/UserController.java
@@ -15,7 +15,7 @@ public interface UserController {
 
     @Tag( description = "User based operations", name = Constants.USER_APIS )
     @Operation( summary = Constants.USER_APIS + "register new user",
-            description = "register new resident or admin",
+            description = "register new resident or admin \n use, userGroupId 1 for admin and 2 for Resident ",
             responses = { @io.swagger.v3.oas.annotations.responses.ApiResponse( responseCode = "200", description = "register new resident or admin" ) } )
     @PostMapping
     ResponseEntity<ApiResponse> registerNewUser(@RequestBody User user);
